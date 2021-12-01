eced4dc69d38dac406e56460ba89090e3e0702de
Cristofer <cristoferlorenzana@gmail.com>
2021-12-01 16:41:47 -0600

    commit tree 9c9e454d936e09e3d79a2faaa50787c80a5431c4

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,24 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
+    words = self.split
+    sentences = 0
+    words.include?("."||"?"||"!") do
+      sentences =+ 1
+    end
+    sentences
 
   end
 end
